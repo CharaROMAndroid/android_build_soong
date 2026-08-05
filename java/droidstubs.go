fc72e0b8c268865cd0404b2722e7419886be2944
Chara <bionichuman07@gmail.com>
2026-08-05 00:03:16 -0400

    droidstubs: Hide InvalidJavadoc documentation issues
    Many AIDL files use {@hide} syntax instead of @hide block tag.
    This generates excessive noise with ErrorWhenNew for little benefit.
    
    Change-Id: I2cc4c4f9a6ec54c5c26a6fcaa3e3faaebef25faf

--- java/droidstubs.go
+++ java/droidstubs.go
@@ -1157,6 +1157,7 @@ var HIDDEN_DOCUMENTATION_ISSUES = []string{
 	"Deprecated",
 	"IntDef",
 	"Nullable",
+	"InvalidJavadoc",
 }
 
 func treatDocumentationIssuesAsWarningErrorWhenNew(cmd *android.RuleBuilderCommand) {
